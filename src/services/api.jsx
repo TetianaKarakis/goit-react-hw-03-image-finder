f9866547c2dc230a5c2eb80952fbe58fbea26580
TetianaKarakis <tetiana.karakis@gmail.com>
2023-08-12 20:11:00 +0300

    /

---
+++ src/services/api.jsx
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const fetchHitsByQuery = async (query, page) => {
+  const response = await axios.get('https://pixabay.com/api/', {
+    method: 'get',
+    params: {
+      key: '37997287-eea6ff72e51878965e89513a4',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 12,
+      page: page,
+    },
+  });
+  return response.data.hits;
+};
